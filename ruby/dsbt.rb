177a58016ca8eb500b43b12d90361bd74848a063
nem <nmccready@gmail.com>
2013-01-29 15:54:34 -0500

    - added mongoscript to run an instance locally with a local data location - mods to sbt ruby script

--- ruby/dsbt.rb
+++ ruby/dsbt.rb
@@ -1,4 +1,5 @@
 portToCheck = ARGV[0]
+
 #dsbt - debug sbt
 #script is meant to run a local copy of ./sbt and provides debugging options to java
 #this is meant to replace environment variable as ports tend to collide on debugging
@@ -10,6 +11,8 @@ if portToCheck == nil
 	print "What is the debug port? "
 	portToCheck = gets.chomp
 end
+puts portToCheck
+
 sbtLines = IO.readlines("./sbt")
 sbtOpts = "${SBT_OPTS}"
 
