8604d7304bd61542e4bf99280fc174cc14b6c36d
nmccready <nemtcan@gmail.com>
2017-03-09 14:52:52 -0500

    updates

---
+++ node/link-all/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "node": true,
+  },
+  "rules": {
+    "no-extra-semi": 1,
+    "semi": 1,
+    "arrow-parens": 0,
+    "strict": 0,
+    "no-unused-vars": [2, { "vars": "all", "args": "none" }],
+    "no-undef": 1
+  },
+  "parserOptions": {
+      "sourceType": "module",
+  }
+};
